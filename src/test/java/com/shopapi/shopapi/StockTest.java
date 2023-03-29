5228b63be06c3586cf667280688a4f42c73780f0
SebastianGuevara <sebastianguve@unisabana.edu.co>
2023-03-28 22:38:13 -0500

    Agrega la prueba de getProducts

--- src/test/java/com/shopapi/shopapi/StockTest.java
+++ src/test/java/com/shopapi/shopapi/StockTest.java
@@ -4,6 +4,7 @@ import com.shopapi.shopapi.data.Stock;
 import com.shopapi.shopapi.repository.IStockRepository;
 import com.shopapi.shopapi.service.StockService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,6 +12,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 @ExtendWith(MockitoExtension.class)
 public class StockTest {
@@ -18,6 +21,7 @@ public class StockTest {
     private IStockRepository stockRepository;
     @InjectMocks
     private StockService stockService;
+
     @Test
     public void Given_a_stock_with_negative_quantity_When_invoking_addProduct_Then_throw_exception(){
         Stock stock = new Stock(1,"Bimbo",3000,-1,new Date());
@@ -28,5 +32,11 @@ public class StockTest {
         Stock stock = new Stock(1,"Bimbo",3000,100,new Date());
         Mockito.when(stockRepository.save(stock)).thenReturn(stock);
         Assertions.assertEquals(stock,stockService.addProduct(stock));
+    }
+    @Test
+    public void Given_nothing_When_invoking_getProducts_Then_show_all_products(){
+        Stock stock = new Stock(1,"Bimbo",3000,100,new Date());
+        Mockito.when(stockRepository.findAll()).thenReturn(Collections.singletonList(stock));
+        Assertions.assertNotNull(stockService.getProducts());
     }
 }
