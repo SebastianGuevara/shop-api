7e25f19cd15614c476d0d383ad107eed5e8f47ef
SebastianGuevara <sebastianguve@unisabana.edu.co>
2023-03-29 22:06:28 -0500

    Agrega test a StockTest

--- src/test/java/com/shopapi/shopapi/StockTest.java
+++ src/test/java/com/shopapi/shopapi/StockTest.java
@@ -4,7 +4,6 @@ import com.shopapi.shopapi.data.Stock;
 import com.shopapi.shopapi.repository.IStockRepository;
 import com.shopapi.shopapi.service.StockService;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,9 +11,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
+import java.util.*;
+
 @ExtendWith(MockitoExtension.class)
 public class StockTest {
     @Mock
@@ -43,22 +41,50 @@ public class StockTest {
     public void Given_negative_stock_When_invoking_updateStock_Then_throw_exception(){
         Integer productCode = 1;
         Integer stockToAdd = -23;
-        RuntimeException exception = Assertions.assertThrows(RuntimeException.class,()->stockService.updateStock(productCode,stockToAdd));
-        Assertions.assertEquals("Cant not add negative stock",exception.getMessage());
+        Stock stock = new Stock(1,"Bimbo",3000,100,new Date());
+        Mockito.when(stockRepository.findById(productCode)).thenReturn(Optional.of(stock));
+        Assertions.assertThrows(RuntimeException.class,()->stockService.updateStock(productCode,stockToAdd));
     }
-    /*@Test
-    public void Given_a_list_of_products_that_are_more_than_the_stock_When_invoking_sellProducts_Then_throw_exception(){
-        Stock stock = new Stock(1,"Bimbo",3000,1000,new Date());
-        Mockito.when(stockRepository.findById(1).get()).thenReturn(stock);
-        //Assertions.assertThrows(RuntimeException.class,()->stockService.sellProducts(stock))
-    }*/
-    /*@Test
+    @Test
+    public void Given_a_list_of_stocks_When_invoking_getTotalPrice_Then_return_total_price(){
+        List<Stock> stocks = new ArrayList<>();
+        Stock stock1 = new Stock(1,"Bimbo",3000,1,new Date());
+        Stock stock2 = new Stock(2,"Kellogs",3000,1,new Date());
+        Stock stock3 = new Stock(3,"Chocorramo",3000,1,new Date());
+        stocks.add(stock1);
+        stocks.add(stock2);
+        stocks.add(stock3);
+        for (Stock stock : stocks){
+            Mockito.when(stockRepository.findById(stock.getID())).thenReturn(Optional.of(stock));
+        }
+        Assertions.assertEquals(9000,stockService.getTotalPrice(stocks));
+    }
+    @Test
     public void Given_stock_to_add_When_invoking_updateStock_Then_add_stock_to_product(){
-        Stock correctStock = new Stock(1,"Bimbo",3000,100,new Date());
-        Stock stock = stockService.updateStock(1,30);
-        Mockito.when(stockRepository.save(correctStock)).thenReturn(correctStock);
-        Assertions.assertNotNull(stockService.updateStock(stock.getID(),30));
+        Stock stock = new Stock(1,"Bimbo",100,100,new Date());
+        Stock newStock = new Stock(1,"Bimbo",130,100,new Date());
+        Integer productCode = 1;
+        Integer stockToAdd = 30;
+        Mockito.when(stockRepository.findById(productCode)).thenReturn(Optional.of(stock));
+        Mockito.when(stockRepository.save(stock)).thenReturn(newStock);
+        Assertions.assertNotNull(stockService.updateStock(productCode,stockToAdd));
+        Assertions.assertEquals(newStock,stockService.updateStock(1,30));
+    }
+    @Test
+    public void Given_a_list_of_products_that_are_more_than_the_stock_When_invoking_sellProducts_Then_throw_exception(){
+        List<Stock> stocks = new ArrayList<>();
+        Stock stock1 = new Stock(1,"Bimbo",3000,1,new Date());
+        Stock stock2 = new Stock(2,"Kellogs",3000,3000,new Date());
+        Stock stock3 = new Stock(3,"Chocorramo",3000,1,new Date());
+        stocks.add(stock1);
+        stocks.add(stock2);
+        stocks.add(stock3);
+        for(Stock stock : stocks){
+            Mockito.when(stockRepository.findById(stock.getID())).thenReturn(Optional.of(stock));
+        }
+        //Mockito.when(stockRepository.findById(1).get()).thenReturn(stock);
+        //Assertions.assertThrows(RuntimeException.class,()->stockService.sellProducts(stock))
+    }
 
-    }*/
 
 }
